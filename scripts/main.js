5bb78cd3a68507fed2b4c8b22d076d2d78256ca4
Vinesh Kannan <vingkan@gmail.com>
2016-01-09 20:12:36 -0600

    Working setInterval Clock

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,19 @@
 var GLOBAL = {
-	SIMULATION_START: 1420178400000,
-	SIMULATION_END: 1421733600000,
+	SIMULATION_START: 1420092000000,
+	SIMULATION_END: 1420956000000,
 	REAL_START: 1420855200000,
 	REAL_END: 1420866000000
 }
 
+var realClock = document.getElementById('real-clock');
+var simulationClock = document.getElementById('simulation-clock');
+
 console.log(moment(GLOBAL.REAL_START).format("[Started at:] M/D hh:mm A"));
 
+/*
+ * Returns scale between real time and simulation time
+ * Multiply real time by this scalar to get simulation time
+ */
 function calculateTimeScale(config){
 	var simulationDuration = config.SIMULATION_END - config.SIMULATION_START;
 	var realDuration = config.REAL_END - config.REAL_START;
@@ -14,9 +21,10 @@ function calculateTimeScale(config){
 	return scale;
 }
 
-var scale = calculateTimeScale(GLOBAL);
-console.log(scale)
-
 window.setInterval(function(){
 	var realNow = new Date().getTime();
+	var scale = calculateTimeScale(GLOBAL);
+	//console.log("Scale: " + scale);
+	realClock.innerHTML = moment(realNow).format("M/D hh:mm A");
+	simulationClock.innerHTML = moment(realNow * scale).format("M/D hh:mm A");
 });
